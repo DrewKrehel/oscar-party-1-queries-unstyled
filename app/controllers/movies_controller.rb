72db101aaad185dae62e6be98ceab9ef7afaeb08
Drew Krehel <drewkrehel@gmail.com>
2025-09-18 01:51:29 +0000

    set up movies detail route

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,11 @@ class MoviesController < ApplicationController
   def index
     @list_of_movies = Movie.all.order({ :year => :desc, :title => :asc })
 
-
     render({ :template => "movies_templates/index"})
+  end
+
+  def show
+
+    render({ :template => "movies_templates/show"})
   end
 end
