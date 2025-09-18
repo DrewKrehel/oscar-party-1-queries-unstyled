67af91c6f72419b6b8758f2f5cd0fc67ca2b45fc
Drew Krehel <drewkrehel@gmail.com>
2025-09-18 01:43:11 +0000

    ordered list and moved logic

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,7 @@
 class MoviesController < ApplicationController
   def index
+    @list_of_movies = Movie.all.order({ :year => :desc, :title => :asc })
+
 
     render({ :template => "movies_templates/index"})
   end
