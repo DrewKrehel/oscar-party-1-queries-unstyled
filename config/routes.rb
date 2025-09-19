2492d0a83de2b1e16396349a1be9890df83dd900
Drew Krehel <drewkrehel@gmail.com>
2025-09-19 01:45:23 +0000

    actors page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
   get("/about", { :controller => "misc", :action => "about" })
+  
   get("/movies", { :controller => "movies", :action => "index" })
   get("/movies/:path_id", { :controller => "movies", :action => "show" })
+  
+  get("/directors", { :controller => "directors", :action => "index" })
+  get("/directors/:path_id", { :controller => "directors", :action => "show" })
+  
   get("/directors", { :controller => "directors", :action => "index" })
   get("/directors/:path_id", { :controller => "directors", :action => "show" })
 end
