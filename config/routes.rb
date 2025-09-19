c60293b3c3ae025dc0a8b20c5da67a6780094fcb
Drew Krehel <drewkrehel@gmail.com>
2025-09-19 01:48:09 +0000

    actors details

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   get("/directors", { :controller => "directors", :action => "index" })
   get("/directors/:path_id", { :controller => "directors", :action => "show" })
   
-  get("/directors", { :controller => "directors", :action => "index" })
-  get("/directors/:path_id", { :controller => "directors", :action => "show" })
+  get("/actors", { :controller => "actors", :action => "index" })
+  get("/actors/:path_id", { :controller => "actors", :action => "show" })
 end
